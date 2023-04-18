e8feb445e52e611c26250db0b118337539cf5341
blysenko1 <b.lysenko@hacken.io>
2023-04-18 10:37:18 +0300

    Added Products page and configured the Pagination

---
+++ src/components/Paginator.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav>
+        <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="javascript:(void(0))" @click="prev">Previous</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:(void(0))" @click="next">Next</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import {watch, ref, SetupContext} from 'vue'
+export default {
+    name: "PaginatorPage",
+    emits: ['page-changed'],
+    props: {
+        lastPage: Number
+    },
+    setup(props: { lastPage: number }, context: SetupContext) {
+        const page = ref(1)
+
+        watch(page, () => {
+            context.emit('page-changed', page.value) // context this variables needs to emit events
+        });
+
+        const prev = () => {
+            if(page.value > 1) {
+                page.value--
+            }
+        }
+
+        const next = () => {
+            if(page.value < props.lastPage.value) {
+                page.value++
+            }
+        }
+        return {
+            next,
+            prev
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/products/Products.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id">
+                    <td>{{ product.id }}</td>
+                    <td><img :src="product.image" width="50"></td>
+                    <td>{{ product.title }}</td>
+                    <td>{{ product.description }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>
+                        <div class="btn-group mr-2">
+                            <router-link :to="`/products/${product.id}/edit`" class="btn btn-sm btm-outline-secondary">Edit</router-link>
+                            <a href="javascript:(void(0))" class="btn btn-sm btm-outline-secondary" @click="del(product.id)">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <Paginator :last-page="lastPage" @page-changed="load($event)"/>
+</template>
+
+<script lang="ts">
+import {onMounted, ref} from 'vue';
+import axios from 'axios';
+import {Product} from "@/models/product";
+import Paginator from "@/components/Paginator.vue";
+
+export default {
+    name: "ProductsList",
+    setup() {
+        const products = ref([])
+        const lastPage = ref(0)
+
+        const load = async(page = 1) => {
+            const {data} = await axios.get(`/products?page=${page}`)
+            products.value = data.data
+            lastPage.value = data.meta.last_page
+        }
+
+        onMounted(load)
+
+        const del = async (id: number) => {
+            if(confirm("Are you sure?")) {
+                await axios.delete(`products/${id}`); // delete from back-side
+
+                products.value = products.value.filter((p: Product) => p.id !== id); // set type of p: to Products - delete from front-side
+            }
+        }
+
+        return {
+            products,
+            lastPage,
+            del,
+            load
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
