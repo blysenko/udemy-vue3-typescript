d7e7cc2eb85fac895feb29a6b0c8ed442bbbaf87
Borys <lysenko.boris25@gmail.com>
2023-06-15 11:17:36 +0300

    Added orders page, configured upload CSV file, Added c3 chart Sales (#8)

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,10 +1,52 @@
 <template>
-  Dashboard
+  <h2>Daily Sales</h2>
+
+  <div id="chart">
+
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import {onMounted} from 'vue';
+import axios from 'axios';
+import * as c3 from 'c3';
+
 export default {
-name: "DashBoard"
+name: "DashBoard",
+  setup() {
+    onMounted(async () => {
+      const chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          colums: [
+              ['x', '2021-1-1', '2021-2-2'],
+              ['sales', 100, 200]
+          ],
+          types: {
+            Sales: 'bar'
+          }
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d'
+            }
+          }
+        }
+      });
+
+      const {data} = await axios.get('chart');
+
+      chart.load({
+        colums: [
+            ['x', ...data.map((r: any) => r.data)],
+            ['Sales', ...data.map((r: any) => r.sum)]
+        ]
+      })
+    });
+  }
 }
 </script>
 
